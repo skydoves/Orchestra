ae48380dbf1c9cf0376f88d92601c80983033c31
skydoves <skydoves@naver.com>
2022-08-28 16:46:27 +0900

    Prepare for the next release

--- buildSrc/src/main/kotlin/com/skydoves/orchestra/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/orchestra/Configuration.kt
@@ -21,10 +21,10 @@ object Configuration {
   const val targetSdk = 33
   const val minSdk = 21
   const val majorVersion = 1
-  const val minorVersion = 1
-  const val patchVersion = 5
+  const val minorVersion = 2
+  const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 15
+  const val versionCode = 16
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
